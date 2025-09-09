89c1049e5f78c39d19399609c6950fe8f894e9ae
Zaryab Zubair <zaryabz043@gmail.com>
2025-09-09 15:39:49 +0500

    auth and db setup done

---
+++ convex/schema.ts
@@ -0,0 +1,41 @@
+import { defineSchema, defineTable } from 'convex/server';
+import { v } from 'convex/values';
+
+export default defineSchema({
+  user: defineTable({
+    userId: v.string(), // clerk userId
+    email: v.string(),
+    name: v.string(),
+    isPro: v.boolean(),
+    proSince: v.optional(v.number()),
+    lemonSqueezyCustomerId: v.optional(v.string()),
+    lemonSqueexyOrderId: v.optional(v.string()),
+  }).index('by_user_id', ['userId']),
+  codeExecution: defineTable({
+    userId: v.string(),
+    language: v.string(),
+    code: v.string(),
+    output: v.optional(v.string()),
+    error: v.optional(v.string()),
+  }),
+  snippet: defineTable({
+    userId: v.string(),
+    userName: v.string(),
+    title: v.string(),
+    language: v.string(),
+    code: v.string(),
+  }).index('by_user_Id', ['userId']),
+  snippetComment: defineTable({
+    snippetId: v.id('snippet'),
+    userId: v.string(),
+    userName: v.string(),
+    content: v.string(),
+  }).index('by_snipped_Id', ['snippetId']),
+  stars: defineTable({
+    userId: v.id('user'),
+    snippetId: v.id('snippet'),
+  })
+    .index('by_user_Id', ['userId'])
+    .index('by_snipped_Id', ['snippetId'])
+    .index('by_user_and_snipped_Id', ['userId', 'snippetId']),
+});
---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import React from 'react';
+
+const page = () => {
+  console.log('Hey from about page');
+  return <div>page</div>;
+};
+
+export default page;
